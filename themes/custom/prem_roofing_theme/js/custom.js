13bec6eec496a09a856ee6879345e82b397fc07c
Amanda Sims (mac) <amanda.sims@gmail.com>
2013-01-27 18:46:28 +1100

    new javascript file custom.js

--- themes/custom/prem_roofing_theme/js/custom.js
+++ themes/custom/prem_roofing_theme/js/custom.js
@@ -1,27 +1,77 @@
 jQuery(document).ready(function($) {
-	var arr = $(".field-name-field-basic-page-image img").map(function() { return this.alt; }).get();
-	$(".field-name-field-basic-page-image .field-item img").each(function (i){
-		$(".field-name-field-basic-page-image .field-item img:eq("+i+")").parent().append("<div>" + arr[i] + "</div>");
- 	});
 
-	$('.field-name-download-as-pdf-link a,.field-name-field-media-link a').click(function() {
-    $(this).attr('target', '_blank');
-	});
-	if(!$('.field-name-download-as-pdf-link a').attr("href")){
-		$('.field-name-download-as-pdf-link a').hide();
-	};
-  var defaultText = "Search site";
-  if ($('#edit-search-block-form--2').val() == '') {
-      $('#edit-search-block-form--2').val(defaultText);
+  var defaultText = "First Name: *";
+  if ($('#edit-submitted-first-name').val() == '') {
+      $('#edit-submitted-first-name').val(defaultText);
   }
-  $('#edit-search-block-form--2').focus(function() { 
+  $('#edit-submitted-first-name').focus(function() { 
     if ($(this).val() == defaultText) {
       $(this).val('');
     }
   });
-  $('#edit-search-block-form--2').blur(function() {
+  $('#edit-submitted-first-name').blur(function() {
     if ($(this).val() == '') {
       $(this).val(defaultText);
+    } 
+  });
+  
+  var defaultText2 = "Last Name: *";
+  if ($('#edit-submitted-last-name').val() == '') {
+      $('#edit-submitted-last-name').val(defaultText2);
+  }
+  $('#edit-submitted-last-name').focus(function() { 
+    if ($(this).val() == defaultText2) {
+      $(this).val('');
+    }
+  });
+  $('#edit-submitted-last-name').blur(function() {
+    if ($(this).val() == '') {
+      $(this).val(defaultText2);
+    } 
+  });
+  
+  var defaultText3 = "Contact number: *";
+  if ($('#edit-submitted-contact-number').val() == '') {
+      $('#edit-submitted-contact-number').val(defaultText3);
+  }
+  $('#edit-submitted-contact-number').focus(function() { 
+    if ($(this).val() == defaultText3) {
+      $(this).val('');
+    }
+  });
+  $('#edit-submitted-contact-number').blur(function() {
+    if ($(this).val() == '') {
+      $(this).val(defaultText3);
+    } 
+  });
+  
+  var defaultText4 = "Email: *";
+  if ($('#edit-submitted-email').val() == '') {
+      $('#edit-submitted-email').val(defaultText4);
+  }
+  $('#edit-submitted-email').focus(function() { 
+    if ($(this).val() == defaultText4) {
+      $(this).val('');
+    }
+  });
+  $('#edit-submitted-email').blur(function() {
+    if ($(this).val() == '') {
+      $(this).val(defaultText4);
+    } 
+  });
+  
+    var defaultText5 = "Message:";
+  if ($('#edit-submitted-message').val() == '') {
+      $('#edit-submitted-message').val(defaultText5);
+  }
+  $('#edit-submitted-message').focus(function() { 
+    if ($(this).val() == defaultText5) {
+      $(this).val('');
+    }
+  });
+  $('#edit-submitted-message').blur(function() {
+    if ($(this).val() == '') {
+      $(this).val(defaultText5);
     } 
   });
 }); 
